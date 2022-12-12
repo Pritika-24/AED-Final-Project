957a83af011ed5c3109ca686b25b2daec4f6a27f
Evi1998 <paul.ev@northeastern.edu>
2022-12-11 19:09:47 -0500

    added request apartments java class

---
+++ StudentHousingApplication/src/Model/Requestapartment.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Evita Alice Paul
+ */
+public class Requestapartment {
+    String ApartmentName;
+    String ApartmentType;
+    int Price;
+    String LandlordName;
+    int ApartmentID;
+
+    public String getApartmentName() {
+        return ApartmentName;
+    }
+
+    public void setApartmentName(String ApartmentName) {
+        this.ApartmentName = ApartmentName;
+    }
+
+    public String getApartmentType() {
+        return ApartmentType;
+    }
+
+    public void setApartmentType(String ApartmentType) {
+        this.ApartmentType = ApartmentType;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int Price) {
+        this.Price = Price;
+    }
+
+    public String getLandlordName() {
+        return LandlordName;
+    }
+
+    public void setLandlordName(String LandlordName) {
+        this.LandlordName = LandlordName;
+    }
+
+    public int getApartmentID() {
+        return ApartmentID;
+    }
+
+    public void setApartmentID(int ApartmentID) {
+        this.ApartmentID = ApartmentID;
+    }
+    
+    
+}
